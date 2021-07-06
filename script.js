e68a2a85f7e2bc42c7e56331721e0bfee631ee77
Johny <joeljoshy143@gmail.com>
2021-07-06 12:12:47 +0530

    Weathe app 1.0

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var input = document.querySelector('.input')
 
 button.addEventListener('click', function (name) {
   let cityname = input.value
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=c638ffb9bb3d3444c71cc6eb04f57f07&units=metric`)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=c638ffb9bb3d3444c71cc6eb04f57f07&units=metric`)
     .then(response => response.json())
     .then(data => {
       console.log(data);
@@ -16,10 +16,14 @@ button.addEventListener('click', function (name) {
       var weath = data['weather'][0]['main']
       var icon = data['weather'][0]['icon']
 
-
-      c_name.innerHTML = "Weather in  "+name
-      c_temp.innerHTML = temp +" °C " 
-      c_weath.innerHTML = weath
+      body = `
+              <h2 id="name">Weather in ${cityname}</h2>
+              <p id="temp">${temp}°C</p>
+              <div>
+                <img src="https://openweathermap.org/img/wn/04n.png" alt="" id="icon" />
+                <p id="weath">${weath}</p>
+              </div>`
+      document.querySelector('.body').innerHTML=body
     
       document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+cityname+"')"
       document.querySelector("#icon").src ="https://openweathermap.org/img/wn/" + icon + ".png";
